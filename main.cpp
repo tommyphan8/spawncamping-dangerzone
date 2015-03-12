348075f385e521253cef28209016732e0dbcfd50
Tommy Phan <tommyphan8@csu.fullerton.edu>
2015-03-11 22:54:43 -0700

    able to download one file

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+		
+	string link1 = "http://bingweb.binghamton.edu/~mgofman1/rbac-pat-slides.ppt.zip";
+	pid_t pid;
+	pid = fork();
+	
+	
+	if (pid == 0)
+	{
+		execlp("/usr/bin/wget","wget", links, NULL);
+	}
+	else if (pid > 0)
+	{
+		
+		wait(NULL);
+		cout << "complete";
+	}
+	else
+	{
+		cout << "fail";
+	}
+	return 0;
+}
