8ef0fd208430d20912585ce3b1168be333637969
Tommy Phan <tommyphan8@csu.fullerton.edu>
2015-03-11 23:35:51 -0700

    able to create multiple children

--- main.cpp
+++ main.cpp
@@ -13,25 +13,45 @@ using namespace std;
 
 int main()
 {
-		
-	string link1 = "http://bingweb.binghamton.edu/~mgofman1/rbac-pat-slides.ppt.zip";
+	string links[2] = {"http://www.cisco.com/networkers/nw04/presos/docs/NMS-1N02.pdf",
+		 "http://www.erlang.org/doc/apps/snmp/snmp.pdf"}; 
+	string link1;
 	pid_t pid;
-	pid = fork();
 	
+	for (int i = 0; i < 2; i++)
+	{
+		link1 = links[i];
+		pid = fork();
+		if (pid == 0)
+		{
+			execlp("/usr/bin/wget","wget", link1.c_str(), NULL);
+		}
+		else if (pid > 0)
+		{
+			
+			wait(NULL);
+			cout << "complete" << link1 << " " << endl;
+		}
+		else
+		{
+			cout << "fail";
+		}
+	}
+	//pid = fork();
 	
-	if (pid == 0)
-	{
-		execlp("/usr/bin/wget","wget", link1.c_str(), NULL);
-	}
-	else if (pid > 0)
-	{
-		
-		wait(NULL);
-		cout << "complete";
-	}
-	else
-	{
-		cout << "fail";
-	}
+	//~ if (pid == 0)
+	//~ {
+		//~ execlp("/usr/bin/wget","wget", link1.c_str(), NULL);
+	//~ }
+	//~ else if (pid > 0)
+	//~ {
+		//~ 
+		//~ wait(NULL);
+		//~ cout << "complete";
+	//~ }
+	//~ else
+	//~ {
+		//~ cout << "fail";
+	//~ }
 	return 0;
 }
